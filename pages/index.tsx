d7a68b5edd937b7f489fa16e6e54469d59245189
Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-28 23:24:16 +0100

    created a useState to manage generating unique components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react"
 import LeftSide from "./components/sides/LeftSide"
 import RightSide from "./components/sides/RightSide"
 import Unique from "./components/experimental/Unique"
@@ -6,12 +6,21 @@ import { nanoid } from 'nanoid'
 
 export default function Home() {
 
+  const [components, setComponents] = useState([<Unique id = {nanoid()}/>]); 
+  
+  function addComponent() { 
+    
+    setComponents([...components, <Unique id = {nanoid()}]) 
+    
+  } 
+
+
   return (
     <div className="grid grid-cols-2">
       <div>
         <LeftSide/>
-        <Unique id ={nanoid()}/>
-        <Unique id ={nanoid()} />
+        <Button onClick={addComponent} text="Call Component"/> 
+        {components.map((item, i) => ( <ListComponent text={item} /> ))} 
       </div>
 
       <div className="hidden lg:block">

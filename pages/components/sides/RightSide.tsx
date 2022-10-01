1ca0892e012f1fdc8ee6d62246508cf867918ab0
Baskin-Ridley <gbaskinridley@gmail.com>
2022-10-01 15:07:58 +0100

    used tailwind to layout grids on right side

--- pages/components/sides/RightSide.tsx
+++ pages/components/sides/RightSide.tsx
@@ -3,6 +3,15 @@ import React from 'react'
 const RightSide = (props: { components: any[] }) => {
   return (
     <div>
+
+      <div className="bg-green-200 h-20"></div>
+      <div className="grid grid-cols-5">
+        <div className="bg-blue-200 col-span-4 h-10"></div>
+        <div className="bg-red-200 col-span-1 h-10"></div>
+      </div>
+
+
+      
       {props.components.map((component) => component)}
     </div>
   )
